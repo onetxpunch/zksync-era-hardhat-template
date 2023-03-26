7f9394929c8d3936ebd9a6c16776001e7d3a4de5
punch <117326984+onetxpunch@users.noreply.github.com>
2023-03-26 01:06:51 -0400

    add mainnet hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,6 +18,15 @@ module.exports = {
       ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
       zksync: true,
     },
+    zkMainnet: {
+      url: "https://zksync2-mainnet.zksync.io",
+      ethNetwork: "mainnet", // Can also be the RPC URL of the Ethereum network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+      // Verification endpoint for mainnet
+      verifyURL:
+        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+      chainId: 324,
+    },
   },
   solidity: {
     version: "0.8.17",
